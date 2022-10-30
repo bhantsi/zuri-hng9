3e2cb32b38b3eee8475dd019b95ba28e5b105e19
Hamza <bhantsi@gmail.com>
2022-10-30 14:52:45 +0100

    Modiefied asset s and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+import './index.css';
+import './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dunia</h1>
+      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="profile-contents">
+            <img src="./public/asset/eha.jpg" alt="User's profile" id="profile__img"/>
+            <h3>Bala Hantsi</h3>
+        </div>
+    )
+}
